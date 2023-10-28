3d5107f0cf8f0fa7c48faea43a1641d98b317c36
Anahit-N <anahit_navoyan@edu.aua.am>
2023-10-28 23:54:32 +0400

    Create DB

--- AB_Testing/DB/CallTracking.sql
+++ AB_Testing/DB/CallTracking.sql
@@ -1,12 +1,11 @@
--- Create Database
+-- Create Database CallTracking
 CREATE DATABASE CallTracking;
 GO
 
--- Use the Created DB
 USE CallTracking;
 GO
 
--- Create DimDate Table
+-- Create tables
 CREATE TABLE DimDate (
     DateID INT PRIMARY KEY,
     CalendarDate DATE,
@@ -16,28 +15,24 @@ CREATE TABLE DimDate (
     Year INT
 );
 
--- Create DimAdvertisement Table
 CREATE TABLE DimAdvertisement (
     AdvertisementID INT PRIMARY KEY,
     AdvertisementType VARCHAR(255),
     Budget DECIMAL(18, 2)
 );
 
--- Create DimSource Table
 CREATE TABLE DimSource (
     SourceID INT PRIMARY KEY,
     SourceName VARCHAR(255),
     SourceType VARCHAR(255)
 );
 
--- Create DimAlgorithm Table
 CREATE TABLE DimAlgorithm (
     AlgorithmID INT PRIMARY KEY,
     AlgorithmName VARCHAR(255),
     Description VARCHAR(MAX)
 );
 
--- Create DimCustomer Table
 CREATE TABLE DimCustomer (
     CustomerID INT PRIMARY KEY,
     CustomerName VARCHAR(255),
@@ -45,7 +40,6 @@ CREATE TABLE DimCustomer (
     ContactInfo VARCHAR(255)
 );
 
--- Create CallTrackingResults Table (Fact Table)
 CREATE TABLE CallTrackingResults (
     ResultID INT PRIMARY KEY,
     DateID INT FOREIGN KEY REFERENCES DimDate(DateID),
@@ -57,5 +51,3 @@ CREATE TABLE CallTrackingResults (
     ConversionStatus VARCHAR(50)
 );
 
-SELECT * FROM DimCustomer
-SELECT * FROM CallTrackingResults
