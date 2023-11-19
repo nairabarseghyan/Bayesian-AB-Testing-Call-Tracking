f248894457e552806dd79210778f48d63281ed3e
Yevaave <yeva_avetisyan@edu.aua.am>
2023-11-19 13:26:18 +0400

    Update bernoulli_algorithm.py and add BernoulliReward.ipynb

--- AB_Testing/models/TS_Bernoulli/bernoulli_algorithm.py
+++ AB_Testing/models/TS_Bernoulli/bernoulli_algorithm.py
@@ -1,8 +1,68 @@
 from AB_Testing.models.base import Bandit
 
-class BernoulliThomsponSampling():
-    def __init__(self, true_mean):
-        super().__init__(true_mean)
-        self.a = 1
-        self.b = 1
-        self.N = 0
+## Bandit Class
+
+class Bandit(ABC):
+
+    @abstractmethod
+    def __init__(self, reward):
+        self.reward = reward
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+## -------------------------------
+
+## ThompsonArm Class
+
+class ThompsonArm(Bandit):
+    """
+    An implementation of the Thompson Sampling algorithm for multi-armed bandit problems.
+
+    Inherits from the Bandit class.
+    """
+    
+    def __init__(self, prev_N = 0, prev_x = 0, reward = 1):
+        """
+        Constructor for the ThompsonSampling class.
+        """
+        self.N = prev_N
+        self.sum_x = prev_x
+        self.reward = reward
+        
+    def __repr__(self):
+        """
+        String representation of the class.
+        """
+        return f"A Bandit with {self.m} Win Rate"
+
+
+    def update(self, x):
+        """
+        Updates the bandit's posterior mean and precision using the reward received.
+        """
+        self.N += 1
+        self.sum_x += x
+
+## -------------------------------
+
+## ThompsonAlgo Class
+
+class ThompsonAlgo:
+    
+    def __init__(self, arms):
+        
+        self.arms = arms
+      
+    
+    def get_best_arm(self):
+        
+        mean = [(m.sum_x / m.N) * m.reward if m.N > 0 else 0 for m in self.arms] 
+        
+        return np.argmax(mean)
+        
