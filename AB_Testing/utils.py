97e59fd6fe6e764b1c2aae8159e256b0f5bc053a
Movsisyan <movsisyan@protonmail.com>
2023-12-03 04:42:16 +0400

    SQL utility parent class

--- AB_Testing/utils.py
+++ AB_Testing/utils.py
@@ -1,1 +1,22 @@
 # Adding non structured functionality
+
+
+class ISQL_Etiquette:
+    """Parent class for sql-facing classes"""
+    def __init__(self):
+        pass
+    
+    def exec(self, query: str, *args, **kwargs):
+        """Executres a given query and commits immediately after"""
+        cur = self.cnxn.execute(query, *args, **kwargs)
+        self.cnxn.commit()
+
+        return cur
+    
+
+    def exec_many(self, query: str, *args, **kwargs):
+        """Executres many queries and commits immediately after"""
+        cur = self.cnxn.executemany(query, *args, **kwargs)
+        self.cnxn.commit()
+
+        return cur
