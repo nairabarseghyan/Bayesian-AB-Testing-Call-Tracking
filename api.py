d3b1098cf5c9a17426eff726512cce9286c8b7f9
Movsisyan <movsisyan@protonmail.com>
2023-12-03 16:23:48 +0400

    Added docstrings

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from typing import Union
 import pandas as pd
 import sqlite3
+import logging
 
 from AB_Testing.data_preperation import SqlHandler
 from AB_Testing.data_preperation.schema import create_ORM
@@ -31,6 +32,14 @@ cnxn = sqlite3.connect(AB_Testing.db_path)
 
 @app.get("/arm")
 async def get_arm(id: int = -1):
+    """Gets arm information, or multiple if id = -1
+
+    Args:
+        id (int, optional): id of arm. Defaults to -1.
+
+    Returns:
+        dict: arm info
+    """    
     with SqlHandler("DimArm") as dim_arm:
         if id < 0:
             res = dim_arm.from_sql_to_pandas().to_dict()
@@ -44,6 +53,16 @@ async def get_arm(id: int = -1):
 
 @app.post("/arm")
 async def add_arm(customer_id: int, type: str, reward: float):
+    """Adds an arm
+
+    Args:
+        customer_id (int): identifier
+        type (str): type
+        reward (float): Amount of reward per trigger
+
+    Returns:
+        dict: Arm as a dict
+    """    
     id = SqlHandler("DimArm").get_next_id()
     ThompsonArm(id, cnxn, type=type, customer_id=customer_id, reward=reward)
 
@@ -52,6 +71,15 @@ async def add_arm(customer_id: int, type: str, reward: float):
     
 @app.put("/arm/type")
 async def mod_arm(id: int, type: Union[str, None]):
+    """Modifies the type of the arm
+
+    Args:
+        id (int): arm identifier
+        type (Union[str, None]): type
+
+    Returns:
+        str: "ok" if ok
+    """    
     try:
         ThompsonArm(id).change_type(type)
     except ValueError as e:
@@ -73,7 +101,16 @@ async def toggle_arm(id: int):
 
 
 @app.get("/sample")
-async def sample_arm(customer_id: int, information: Union[str, None]):
+async def sample_arm(customer_id: int, information: Union[str, None]) -> dict:
+    """Samples from all arms and choses the best one
+
+    Args:
+        customer_id (int): Customer id
+        information (Union[str, None]): Additional client data
+
+    Returns:
+        dict: a serve object as a dict
+    """    
     algo = ThompsonAlgo(cnxn, customer_id)
     serve = algo.get_best_arm(information)
 
@@ -81,7 +118,17 @@ async def sample_arm(customer_id: int, information: Union[str, None]):
 
 
 @app.put("/sample")
-async def log_result(customer_id, arm_id, serve_id):
+async def log_result(customer_id, arm_id, serve_id) -> str:
+    """Logs a trigger
+
+    Args:
+        customer_id (int): Customer identifier
+        arm_id (int): Arm identifier
+        serve_id (int): Serve identifier
+
+    Returns:
+        str: "ok" of ok
+    """    
     algo = ThompsonAlgo(cnxn, customer_id)
     [arm for arm in algo.arms if arm.id == int(arm_id)][0].log_trigger(serve_id)
     return "ok"
@@ -90,6 +137,14 @@ async def log_result(customer_id, arm_id, serve_id):
 
 @app.get("/stats")
 async def stats(arm_id: int):
+    """Summary statistics and state of an arm
+
+    Args:
+        arm_id (int): arm identifier
+
+    Returns:
+        dict: Dict with aggregated values and state
+    """    
     agr = SqlHandler("AggregateResult")
     res = agr.select_one(arm_id)
     return res
@@ -98,6 +153,16 @@ async def stats(arm_id: int):
 
 @app.post("/client")
 async def add_client(name:str, location: str, contact: str):
+    """Adds a client to the db
+
+    Args:
+        name (str): Name
+        location (str): location
+        contact (str): contact info
+
+    Returns:
+        dict: customer as a dict
+    """    
     cust = SqlHandler("DimCustomer")
     id = cust.get_next_id()
     cust_dict = dict(customer_id=id, name=name, location=location, contact=contact)
