163789ae702225592dd436da016343d3e0260fb1
Movsisyan <movsisyan@protonmail.com>
2023-12-03 04:55:34 +0400

    restructuring

--- AB_Testing/data_preperation/__init__.py
+++ AB_Testing/data_preperation/__init__.py
@@ -1,3 +1,4 @@
 from ..logger import CustomFormatter
 from .data_generator import generate_advertisment, generate_customer, generate_source, generate_date, generate_calls
 from .sql_interactions import SqlHandler
+from .schema import create_ORM
--- AB_Testing/models/TS_Bernoulli/__init__.py
+++ AB_Testing/models/TS_Bernoulli/__init__.py
@@ -1,4 +1,4 @@
 
-from .bernoulli_algorithm import BernoulliThomsponSampling
+from .bernoulli_algorithm import ThompsonAlgo, ThompsonArm
 
 
